ee0e96e15b601920f0b2ae7e705d5674a41352a8
Cedric Creusot <cedric.creusot@gmail.com>
2024-01-29 06:56:30 +0100

    Add first test with kotest, and using a state to run the timer

--- src/main/kotlin/fr/cedriccreusot/viewmodel/PomodoroViewModel.kt
+++ src/main/kotlin/fr/cedriccreusot/viewmodel/PomodoroViewModel.kt
@@ -1,4 +1,18 @@
 package fr.cedriccreusot.viewmodel
 
-class PomodoroViewModel {
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
+
+sealed class PomodoroState(open val duration: Duration) {
+    data class Idle(override val duration: Duration) : PomodoroState(duration)
+    data class Running(override val duration: Duration) : PomodoroState(duration)
+    data class Paused(override val duration: Duration) : PomodoroState(duration)
+    object Finished : PomodoroState(0L.milliseconds)
+}
+
+class PomodoroViewModel(private val duration: Duration) {
+    private val _state = MutableStateFlow(PomodoroState.Idle(duration))
+    val state = _state.asStateFlow()
 }
--- src/test/kotlin/fr/cedriccreusot/viewmodel/PomodoroViewModelTest.kt
+++ src/test/kotlin/fr/cedriccreusot/viewmodel/PomodoroViewModelTest.kt
@@ -1,5 +1,17 @@
 package fr.cedriccreusot.viewmodel
 
-class PomodoroViewModelTest {
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.flow.collect
+import kotlin.time.Duration.Companion.milliseconds
 
+class PomodoroViewModelTest : FunSpec() {
+    init {
+        test("Given a duration is set in milliseconds, when subscribe to the timer view model, it should return a state that is not started with the same start duration").config(coroutineTestScope = true) {
+            val viewModel = PomodoroViewModel(duration = 1_000L.milliseconds)
+
+            val state = viewModel.state.collect() shouldBe PomodoroState.Idle(1_000L.milliseconds)
+        }
+
+    }
 }
