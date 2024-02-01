3ceac2e090e93b266b70c97fb4541c4fd80b8ad6
Cédric Creusot <cedric.creusot@gmail.com>
2024-02-01 15:06:04 +0100

    Add test en fix test

--- src/main/kotlin/fr/cedriccreusot/viewmodel/PomodoroViewModel.kt
+++ src/main/kotlin/fr/cedriccreusot/viewmodel/PomodoroViewModel.kt
@@ -4,6 +4,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.minutes
 
 sealed class PomodoroState(open val duration: Duration) {
     data class Idle(override val duration: Duration) : PomodoroState(duration)
@@ -12,7 +13,8 @@ sealed class PomodoroState(open val duration: Duration) {
     object Finished : PomodoroState(0L.milliseconds)
 }
 
-class PomodoroViewModel(private val duration: Duration) {
+class PomodoroViewModel(duration: Duration = 25.minutes, val pomodoroMax: Int = 4) {
+
     private val _state = MutableStateFlow(PomodoroState.Idle(duration))
     val state = _state.asStateFlow()
 }
--- src/test/kotlin/fr/cedriccreusot/viewmodel/PomodoroViewModelTest.kt
+++ src/test/kotlin/fr/cedriccreusot/viewmodel/PomodoroViewModelTest.kt
@@ -2,16 +2,29 @@ package fr.cedriccreusot.viewmodel
 
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
-import kotlinx.coroutines.flow.collect
 import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.minutes
 
 class PomodoroViewModelTest : FunSpec() {
     init {
+        test("Given the pomodorule, when a PomodoroViewModel is created, it should return a state that is not started with the default duration and have the number of pomodoro max defined to 4").config(coroutineTestScope = true) {
+            val viewModel = PomodoroViewModel()
+
+            viewModel.state.value shouldBe PomodoroState.Idle(25.minutes)
+            viewModel.pomodoroMax shouldBe 4
+        }
+
+        test("Given a number of pomodoro is set to 2, when a PomodoroViewModel is created, it should return a state that is not started with the default duration and have the number of pomodoro max defined to 2").config(coroutineTestScope = true) {
+            val viewModel = PomodoroViewModel(pomodoroMax = 2)
+
+            viewModel.state.value shouldBe PomodoroState.Idle(25.minutes)
+            viewModel.pomodoroMax shouldBe 2
+        }
+
         test("Given a duration is set in milliseconds, when subscribe to the timer view model, it should return a state that is not started with the same start duration").config(coroutineTestScope = true) {
             val viewModel = PomodoroViewModel(duration = 1_000L.milliseconds)
 
-            val state = viewModel.state.collect() shouldBe PomodoroState.Idle(1_000L.milliseconds)
+            viewModel.state.value shouldBe PomodoroState.Idle(1_000L.milliseconds)
         }
-
     }
 }
