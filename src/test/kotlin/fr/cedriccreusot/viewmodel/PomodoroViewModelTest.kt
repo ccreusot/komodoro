d2a86dec1837d21f900ded28dd68df7207952f4e
Cedric Creusot <cedric.creusot@gmail.com>
2024-02-09 11:13:40 +0100

    Remove unused import and launch in the test

--- src/test/kotlin/fr/cedriccreusot/viewmodel/PomodoroViewModelTest.kt
+++ src/test/kotlin/fr/cedriccreusot/viewmodel/PomodoroViewModelTest.kt
@@ -2,16 +2,11 @@ package fr.cedriccreusot.viewmodel
 
 import app.cash.turbine.test
 import io.kotest.core.spec.style.FunSpec
-import io.kotest.core.test.testCoroutineScheduler
 import io.kotest.matchers.shouldBe
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlin.time.Duration.Companion.milliseconds
 import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
 
-@OptIn(ExperimentalStdlibApi::class, ExperimentalCoroutinesApi::class)
 class PomodoroViewModelTest : FunSpec() {
     init {
         test("Given the pomodorule, when a PomodoroViewModel is created, it should return a state that is not started with the default duration and have the number of pomodoro max defined to 4").config(
@@ -44,19 +39,11 @@ class PomodoroViewModelTest : FunSpec() {
             coroutineTestScope = true
         ) {
             val viewModel = PomodoroViewModel(duration = 3.seconds)
-            val dispatcher = UnconfinedTestDispatcher(testCoroutineScheduler)
-
-            // I am trying to test like this because it will likely be this way in the future
-            val job = launch(dispatcher) {
-                viewModel.state.test(timeout = 4.seconds) {
-                    skipItems(1)
-                    awaitItem() shouldBe PomodoroState.Pomodoro.Running(2.seconds)
-                }
+            viewModel.state.test(timeout = 4.seconds) {
+                viewModel.start()
+                skipItems(1)
+                awaitItem() shouldBe PomodoroState.Pomodoro.Running(2.seconds)
             }
-
-            viewModel.start()
-
-            job.join()
         }
 
         test("Given the viewModel is initialized, when we start the pomodoro, it should return a state Finished when all the time is elapsed").config(
